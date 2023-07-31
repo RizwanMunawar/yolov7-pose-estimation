50e58407404c1cded8cc308138b6387cef4511c2
kunzhi-yu <yukunzhi@Hotmail.com>
2023-07-31 15:41:03 -0400

    adding background sub

--- webstreaming_V2.py
+++ webstreaming_V2.py
@@ -88,28 +88,158 @@ def return_frame(anonymize=True, device='cpu', min_area=2000, thresh_val=25, yol
 
     while cap.isOpened:
         with lock:  # wait until the lock is acquired. We need to acquire the lock to ensure the frame variable is not accidentally being read by a client while we are trying to update it.
-            success, frame = cap.read()  # read the camera frame
+            success, cap_frame = cap.read()  # read the camera frame
             if not success:
                 break
+
+            print("Frame {} Processing".format(frame_count + 1))
+            fps_start_time = time.time()  # start time for fps calculation
+
+            # Background subtraction and YOLO frame prep
+            curr_grey_frame = background_sub_frame_prep(cap_frame)
+            curr_frame = yolo_frame_prep(device, cap_frame)
+
+            if anonymize:
+                im0 = init_background.copy()
             else:
-                stream_img = background_sub_frame_prep(frame)
-                (flag, encodedImage) = cv2.imencode(".jpg", stream_img)  # encode the frame in JPEG format
-                if not flag:  # ensure the frame was successfully encoded
-                    continue
+                # The background will be the current frame
+                im0 = curr_frame[0].permute(1, 2, 0) * 255  # Change format [b, c, h, w] to [h, w, c]
+                im0 = im0.cpu().numpy().astype(np.uint8)
+                im0 = cv2.cvtColor(im0, cv2.COLOR_RGB2BGR)  # reshape image format to (BGR)
+
+                # Perform background subtraction
+                processed_frame, static_count = run_background_sub(init_background_grey, curr_grey_frame,
+                                                                   prev_grey_frame, static_count, im0)
+                is_motion = processed_frame.get_is_motion
+
+            (flag, encodedImage) = cv2.imencode(".jpg", curr_grey_frame)  # encode the frame in JPEG format
+            if not flag:  # ensure the frame was successfully encoded
+                continue
 
         yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(
             encodedImage) + b'\r\n')  # yield some text and the output frame in the byte format
 
 
+def update_df(bed_occupied, date_time, df, is_motion, num_detections, frame_count, fps):
+    """Updates the dataframe df with details from the current frame
+    """
+    if frame_count % int(fps) == 0:
+        new_row = {'date': date_time.strftime("%Y-%m-%d"), 'time': date_time.strftime("%H:%M:%S"), 'motion': is_motion,
+                   'yolo_detections': int(num_detections), 'bed_occupied': bool(bed_occupied)}
+        df.loc[len(df)] = new_row
+
+
+def place_txt_results(bed_occupied, is_motion, num_detections, processed_frame):
+    """Places the text of the results onto the processed frame.
+    """
+    cv2.putText(processed_frame, "Motion: {}".format(is_motion), (10, 40),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+    cv2.putText(processed_frame, "YOLO detections: {}".format(num_detections), (10, 60),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+    cv2.putText(processed_frame, "Bed occupied: {}".format(bed_occupied), (10, 80),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+
+    # put timestamp
+    dt = datetime.now()
+    cv2.putText(processed_frame, dt.strftime("%Y-%m-%d %H:%M:%S"), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                (255, 255, 255), 1)
+
+    return dt
+
+
+def run_background_sub(background_grey, curr_grey_frame, prev_grey_frame, static_count, curr_color_frame):
+    """
+    Returns a frame with the background subtraction completed and labeled, the current grey frame to serve as the new
+    background, and the updated static counter.
+    1) computes difference in the frame from the original background and previous frame
+    2) thresholding to filter areas with significant change in pixel values
+    3) highlights the areas with motion in white
+    """
+    is_motion = False
+    prev_diff = False
+
+    # compute the  difference
+    frame_delta = cv2.absdiff(background_grey, curr_grey_frame)
+    prev_delta = cv2.absdiff(prev_grey_frame, curr_grey_frame)
+
+    # pixels are either 0 or 255.
+    thresh = cv2.threshold(frame_delta, opt.thresh_val, 255, cv2.THRESH_BINARY)[1]
+    prev_thresh = cv2.threshold(prev_delta, opt.thresh_val, 255, cv2.THRESH_BINARY)[1]
+
+    # find the outlines of the white parts from background
+    thresh = cv2.dilate(thresh, None, iterations=3)  # size of foreground increases
+    curr_contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    curr_contours = imutils.grab_contours(curr_contours)
+
+    # find outlines from previous frame
+    prev_thresh = cv2.dilate(prev_thresh, None, iterations=2)
+    prev_contours = cv2.findContours(prev_thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    prev_contours = imutils.grab_contours(prev_contours)
+
+    for c in curr_contours:
+        # Only care about contour if it's larger than the min
+        if cv2.contourArea(c) >= opt.min_area:
+            is_motion = True
+            break
+
+    for c in prev_contours:
+        if cv2.contourArea(c) >= opt.min_area:
+            prev_diff = True
+            static_count = 0
+            break
+
+    if not prev_diff:
+        static_count += 1
+
+    thresh_color = cv2.cvtColor(thresh, cv2.COLOR_GRAY2RGB)
+    overlay = cv2.addWeighted(curr_color_frame, 0.75, thresh_color, 0.25, 0)
+
+    overlay = frame.ProcessedFrame(overlay, is_motion)
+
+    return overlay, static_count
+
+
+def yolo_output_plotter(background, names, output_data):
+    """
+    Plots the yolo model outputs onto background. Calculates the number of detections and places them on the background.
+    Returns the processed frame.
+    """
+    # if there are no poses, then there is no one on the bed
+    bed_occupied = False
+    n = 0
+
+    for i, pose in enumerate(output_data):  # detections per image
+        if len(output_data) and len(pose[:, 5].unique()) != 0:  # check if no pose
+            for c in pose[:, 5].unique():  # Print results
+                n = (pose[:, 5] == c).sum()  # detections per class
+                # "YOLO detections: {}".format(n)
+
+            for det_index, (*xyxy, conf, cls) in enumerate(
+                    reversed(pose[:, :6])):  # loop over poses for drawing on frame
+                c = int(cls)  # integer class
+                keypoints = pose[det_index, 6:]
+                label = None if opt.hide_labels else (
+                    names[c] if opt.hide_conf else f'{names[c]} {conf:.2f}')
+
+                bed_occupied = plot_one_box_kpt(xyxy, background, label=label, color=colors(c, True),
+                                                line_thickness=opt.line_thickness, kpt_label=True, kpts=keypoints,
+                                                steps=3,
+                                                orig_shape=background.shape[:2])
+
+    processed_frame = frame.ProcessedFrame(background, True, n, bed_occupied)
+
+    return processed_frame
+
+
 """
 Function to use the flask function Response
 """
-
-
 @app.route("/video_feed")
 def video_feed():
     return Response(return_frame(),
                     mimetype="multipart/x-mixed-replace; boundary=frame")  # MIME type a.k.a. media type: indicates the nature and format of a document, file, or assortment of bytes. MIME types are defined and standardized in IETF's RFC6838.
+
+
 
 
 def parse_opt():
